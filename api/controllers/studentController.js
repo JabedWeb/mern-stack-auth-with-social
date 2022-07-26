607e76ab3518106e2215b7c2d0d8c17f8b6a6e07
jabed366 <jabedhasan231@gmail.com>
2022-07-26 12:34:17 +0600

    api method

---
+++ api/controllers/studentController.js
@@ -0,0 +1,47 @@
+/**
+ * @public
+ * @route /api/student
+ * @method GET
+ */
+
+export const getAllStudents =(req,res)=>{
+    res.send('I am from all students')
+}
+/**
+ * @public
+ * @route /api/student
+ * @method POST
+ */
+
+export const getSingleStudent =(req,res)=>{
+    res.send('I am from single students')
+}
+/**
+ * @public
+ * @route /api/student/:id
+ * @method POST
+ */
+
+ export const createStudent =(req,res)=>{
+    res.send('I am from create students')
+}
+
+/**
+ * @public
+ * @route /api/student/:id
+ * @method PUT/PATCH
+ */
+
+export const updateStudent =(req,res)=>{
+    res.send('I am from update students')
+}
+/**
+ * @public
+ * @route /api/student/:id
+ * @method DELETE
+ */
+
+export const deleteStudent =(req,res)=>{
+    res.send('I am from all students')
+}
+
--- api/server.js
+++ api/server.js
@@ -2,13 +2,24 @@ import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
 
+import studentRoute from './routes/student.js';
+
+
+
 
 //init express
 const app =express();
 dotenv.config();
 
+//middleware
+app.use(express.json())
+app.use(express.json(express.urlencoded({extended:false})))
+
 //int env variables
 const PORT=process.env.SERVER_PORT || 5050;
+
+//routes
+app.use('/api/student',studentRoute)
 
 //listen server
 app.listen(PORT,()=>{
